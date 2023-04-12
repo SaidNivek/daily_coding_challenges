f41faaed019fef77118eef1c9d3d6c3d92e0adb3
SaidNivek <kevin.vi.dias@gmail.com>
2023-04-11 20:52:58 -0400

    clean up console.logs

--- challenges.js
+++ challenges.js
@@ -441,15 +441,14 @@ function mumble(str) {
         if(strArr.length > 2) {
             results += '-'
         }
-    }
-    
+    }    
     return results
 }
 
-console.log(mumble('X')); //=> 'X'
-console.log(mumble('abc')); //=> 'a-bb-ccc'
-console.log(mumble('121')); //=> '1-22-111'
-console.log(mumble('!A 2')); //=> '!-AA-   -2222'
+// console.log(mumble('X')); //=> 'X'
+// console.log(mumble('abc')); //=> 'a-bb-ccc'
+// console.log(mumble('121')); //=> '1-22-111'
+// console.log(mumble('!A 2')); //=> '!-AA-   -2222'
 
 
 
